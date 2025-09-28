a5d982c786221c33da98f44caec05e395bb7a142
krzysztof-bdi <krzysztof@baltic-digital.org>
2025-09-28 14:44:19 +0200

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.html",
+    "./src/**/*.{html,js}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+      transitionProperty: {
+        height: "height",
+      },
+    },
+  },
+  plugins: [],
+};
